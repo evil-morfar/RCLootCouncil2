8ca7048f784440ded50e6ec9e56b5bb96b24389a
evil-morfar <evil_morfar@hotmail.com>
2016-03-22 00:15:05 +0100

    Prepped for alpha11

--- core.lua
+++ core.lua
@@ -53,7 +53,7 @@ function RCLootCouncil:OnInitialize()
   	self.version = GetAddOnMetadata("RCLootCouncil2", "Version")
 	self.nnp = false
 	self.debug = false
-	self.tVersion = "Alpha.10" -- String or nil. Indicates test version, which alters stuff like version check. Is appended to 'version', i.e. "version-tVersion"
+	self.tVersion = "Alpha.11" -- String or nil. Indicates test version, which alters stuff like version check. Is appended to 'version', i.e. "version-tVersion"
 
 	self.playerClass = select(2, UnitClass("player"))
 	self.guildRank = L["Unguilded"]
