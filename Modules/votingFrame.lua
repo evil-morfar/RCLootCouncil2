a10f046664b1da9466de467c53cd67b9a4d3f4b1
evil-morfar <evil_morfar@hotmail.com>
2017-03-20 18:22:43 +0100

    Extra frame checks

--- Modules/votingFrame.lua
+++ Modules/votingFrame.lua
@@ -309,6 +309,7 @@ end
 --	Visuals														--
 ------------------------------------------------------------------
 function RCVotingFrame:Update()
+	if not self.frame then return end -- No updates when it doesn't exist
 	self.frame.st:SortData()
 	-- update awardString
 	if lootTable[session] and lootTable[session].awarded then
