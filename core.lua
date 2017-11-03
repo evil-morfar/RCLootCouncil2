d5a844110c7479e7282d66c2ebfa241ccf356a58
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-11-02 22:43:51 -0700

    Record self.lootMethod

--- core.lua
+++ core.lua
@@ -1435,6 +1435,7 @@ end
 function RCLootCouncil:NewMLCheck()
 	local old_ml = self.masterLooter
 	self.isMasterLooter, self.masterLooter = self:GetML()
+	self.lootMethod = GetLootMethod()
 	if IsPartyLFG() then return end	-- We can't use in lfg/lfd so don't bother
 	if self.masterLooter and self.masterLooter ~= "" and strfind(self.masterLooter, "Unknown") then
 		-- ML might be unknown for some reason
