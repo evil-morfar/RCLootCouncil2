a3ec077f89147702fc8042fff3f5b1457868ea26
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-11-09 17:09:25 -0800

    Fix

--- Modules/lootHistory.lua
+++ Modules/lootHistory.lua
@@ -347,23 +347,21 @@ function LootHistory:ExportHistory()
 	if export and export ~= "" then -- do something
 		--debugprofilestart()
 		if export:len() < 40000 then
-			self.frame.exportFrame.edit.editBox:SetScript("OnChar", nil)
+			self.frame.exportFrame.edit:SetCallback("OnTextChanged", function(self)
+				self:SetText(export)
+			end)
 			self.frame.exportFrame.edit:SetText(export)
 			self.frame.exportFrame.edit:HighlightText()
 			self.frame.exportFrame.edit:SetFocus()
 			self.frame.exportFrame:Show()
-			self.frame.exportFrame.edit.editBox:SetScript("OnChar", function(self)
+		else -- Use hugeExportFrame(Single line editBox) for large export to avoid freezing the game.
+			self.frame.hugeExportFrame.edit:SetCallback("OnTextChanged", function(self)
 				self:SetText(export)
 			end)
-		else -- Use hugeExportFrame(Single line editBox) for large export to avoid freezing the game.
-			self.frame.hugeExportFrame.edit:SetCallback("OnTextChanged", nil)
 			self.frame.hugeExportFrame.edit:SetText(export)
 			self.frame.hugeExportFrame.edit:HighlightText()
 			self.frame.hugeExportFrame.edit:SetFocus()
 			self.frame.hugeExportFrame:Show()
-			self.frame.hugeExportFrame.edit:SetCallback("OnTextChanged", function(self)
-				self:SetText(export)
-			end)
 		end
 		--addon:Debug("Display time:", debugprofilestop(), "ms")
 	end
