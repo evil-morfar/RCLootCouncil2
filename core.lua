2bb387d07ea203a3d9f41c39ab3b64e387f84e62
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-11-10 23:25:54 -0800

    Use UnitIsUnit instead of ~=   Dont want to have name problems.

--- core.lua
+++ core.lua
@@ -621,14 +621,23 @@ function RCLootCouncil:SendCommand(target, command, ...)
 									-- Only send data to council, for security.
 		if self.mldb.observe or #self.council >= GetNumGroupMembers() then -- Everyone can see the voting frame, just send to the entire group
 			self:SendCommand("group", command, ...)
-		elseif not tContains(self.council, self.masterLooter) then -- council is invalid because it does not contain ML, just send to the entire group so we don't break stuffs.
-			self:Debug("SendCommand to council, but council does not contain ML", self.masterLooter)
-			self:SendCommand("group", command, ...)
 		else
-			self:SendCommand(self.masterLooter, command, ...) -- Send to ML first
+			local mlInCouncil = false
 			for _, v in ipairs(self.council) do
-				if v ~= self.masterLooter then -- Then other councilmen
-					self:SendCommand(v, command, ...)
+				if self:UnitIsUnit(v, self.masterLooter) then
+					mlInCouncil = true
+					break
+				end
+			end
+			if not mlInCouncil then -- council is invalid because it does not contain ML, just send to the entire group so we don't break stuffs.
+				self:Debug("SendCommand to council, but council does not contain ML", self.masterLooter)
+				self:SendCommand("group", command, ...)
+			else
+				self:SendCommand(self.masterLooter, command, ...) -- Send to ML first
+				for _, v in ipairs(self.council) do
+					if not self:UnitIsUnit(v, self.masterLooter) then -- Then other councilmen
+						self:SendCommand(v, command, ...)
+					end
 				end
 			end
 		end
