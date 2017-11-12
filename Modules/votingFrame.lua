2fd1a9a0a32ff788d65152932b96970019ac82a0
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-11-12 15:59:06 -0800

    Only ML should send roll

--- Modules/votingFrame.lua
+++ Modules/votingFrame.lua
@@ -306,7 +306,11 @@ function RCVotingFrame:Setup(table)
 	session = 1
 	self:BuildST()
 	self:SwitchSession(session)
-	if db.autoAddRolls then for i = 1, #lootTable do self:DoRandomRolls(i) end end
+	if addon.isMasterLooter and db.autoAddRolls then 
+		for i = 1, #lootTable do 
+			self:DoRandomRolls(i) 
+		end 
+	end
 end
 
 function RCVotingFrame:HandleVote(session, name, vote, voter)
