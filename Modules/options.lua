40baaa04c5c96f87fe51e8b4cd0669f944cac7ae
evil-morfar <evil_morfar@hotmail.com>
2017-11-24 18:23:13 +0100

    Removed spacer

--- Modules/options.lua
+++ Modules/options.lua
@@ -510,13 +510,8 @@ function addon:OptionsTable()
 										type = "toggle",
 										disabled = function() return not self.db.profile.autoLoot end,
 									},
-									spacer2 = {
-										order = 8,
-										type = "header",
-										name = "",
-									},
 									autolootOthersBoE = {
-										order = 9,
+										order = 8,
 										name = L["Autoloot all BoE"],
 										desc = L["autoloot_others_BoE_desc"],
 										type = "toggle",
