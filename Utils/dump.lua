5b58e9db7e4f5e418a335f698f1ffa45e9e6b5e9
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-12-07 04:31:58 -0800

    Create a generic version of export frame.

--- Utils/dump.lua
+++ Utils/dump.lua
@@ -6,6 +6,8 @@
 if LibDebug then LibDebug() end
 --@end-debug@
 
+local AG = LibStub("AceGUI-3.0")
+
 local ipairs = ipairs
 local math = math
 local pairs = pairs
@@ -99,4 +101,72 @@ function RCLootCouncil:DumpSavedVariables()
 	local nres, res = self:DumpLuaFormat(RCLootCouncilDB, "RCLootCouncilDB", res, 2)
 	local nres, res = self:DumpLuaFormat(RCLootCouncilLootDB, "RCLootCouncilLootDB", res, nres+1)
 	return nres, res
+end
+
+-- Export text for copy & paste.
+-- Currently redundant with the code in LootHistory.
+-- Should simplify the code in LootHistory later.
+do 
+	-- Export frame
+	local exportFrame = AG:Create("Window")
+	exportFrame:SetLayout("List")
+	exportFrame:SetWidth(700)
+	exportFrame:SetHeight(360)
+	exportFrame:Hide()
+
+	exportFrame.desc = AG:Create("Label")
+	exportFrame.desc:SetRelativeWidth(1)
+	exportFrame:AddChild(exportFrame.desc)
+	
+	exportFrame.edit = AG:Create("MultiLineEditBox")
+	exportFrame.edit:SetNumLines(20)
+	exportFrame.edit:SetFullWidth(true)
+	exportFrame.edit:SetLabel("")
+	exportFrame:AddChild(exportFrame.edit)
+
+	-- Frame for huge export. Use single line editbox to avoid freezing.
+	local hugeExportFrame = AG:Create("Window")
+	hugeExportFrame:SetLayout("List")
+	hugeExportFrame:SetWidth(700)
+	hugeExportFrame:SetHeight(100)
+	hugeExportFrame:Hide()
+
+	hugeExportFrame.desc = AG:Create("Label")
+	hugeExportFrame.desc:SetRelativeWidth(1)
+	hugeExportFrame:AddChild(hugeExportFrame.desc)
+
+	hugeExportFrame.edit = AG:Create("EditBox")
+	hugeExportFrame.edit:SetFullWidth(true)
+	hugeExportFrame.edit:SetMaxLetters(0)
+	hugeExportFrame.edit:SetLabel("")
+	hugeExportFrame:AddChild(hugeExportFrame.edit)
+
+	--@param text: The text to export
+	--@param title: Optional. The title of the frame.
+	--@param desc: The description. Long word is wraped.
+	function RCLootCouncil:ExportText(text, title, desc)
+		if text:len() < 40000 then
+			exportFrame:Show()
+			exportFrame:SetTitle(title or "")
+			exportFrame.desc:SetText(desc or "")
+			exportFrame.edit:SetCallback("OnTextChanged", function(self)
+				self:SetText(text)
+			end)
+			exportFrame.edit:SetText(text)
+			exportFrame.edit:SetFocus()
+			exportFrame.edit:HighlightText()
+		else -- Use hugeExportFrame(Single line editBox) for large export to avoid freezing the game.
+			hugeExportFrame:Show()
+			hugeExportFrame:SetTitle(title or "")
+			hugeExportFrame.desc:SetText(desc or "")
+			hugeExportFrame.edit:SetCallback("OnTextChanged", function(self)
+				self:SetText(text)
+			end)
+			hugeExportFrame.edit:SetText(text)
+			hugeExportFrame.edit:SetFocus()
+			hugeExportFrame.edit:HighlightText()
+		end
+
+	end
+
 end
