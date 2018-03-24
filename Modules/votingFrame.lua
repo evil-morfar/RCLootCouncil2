f70c4174685a245a9bd0e711f2e62320f61d549c
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2018-03-24 21:07:01 +0800

    Name column shows out of instance

--- Modules/votingFrame.lua
+++ Modules/votingFrame.lua
@@ -935,6 +935,13 @@ function RCVotingFrame.SetCellName(rowFrame, frame, data, cols, row, realrow, co
 	local name = data[realrow].name
 	frame.text:SetText(addon.Ambiguate(name))
 	local c = addon:GetClassColor(lootTable[session].candidates[name].class)
+	if (IsInInstance() and select(4, UnitPosition("player")) ~= select(4, UnitPosition(Ambiguate(name, "short"))))
+			-- Mark as out of instance if the current player is in an instance and the raider is in other instancemap
+			or ((not IsInInstance) and UnitPosition(Ambiguate(name, "short")) ~= nil) then
+			-- If the current player is not in an instance, mark as out of instance if 1st return of UnitPosition is not nil
+			-- This function returns nil if the raider is in any instance.
+			c = c.." (Out of instance)"
+	end
 	frame.text:SetTextColor(c.r, c.g, c.b, c.a)
 	data[realrow].cols[column].value = name or ""
 end
