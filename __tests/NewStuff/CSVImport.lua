cfeb04c0e48bbe76adf1bff071cbc3dc28b00484
evil_morfar <evil_morfar@hotmail.com>
2019-11-22 05:56:50 +0100

    A LOT more work on the CSVImporter

--- __tests/NewStuff/CSVImport.lua
+++ __tests/NewStuff/CSVImport.lua
@@ -15,6 +15,17 @@ function His:DetermineImportType (import)
 
    -- Check csv
    if import:sub(1, 6) == "player" then
+      -- Check for Sheet Copy
+      if import:sub(7,7) == "\t" then
+         if import:find("\tboss\tgear1") then -- Standard tsv export - not supported
+            return "tsv"
+         end
+         import = import:gsub("\t", ",")
+         if import:find("boss,difficultyID,mapID,") then
+            return "tsv-csv" -- Copied from Google Sheet
+         end
+         return "Unknown"
+      end
       return "csv"
    elseif import:sub(1, 2) == "^1" then
       return "playerexport"
@@ -24,16 +35,26 @@ function His:DetermineImportType (import)
 end
 
 function His:ImportCSV (import)
+   return self:ImportNew(import, ",")
+end
+
+function His:ImportTSV_CSV (import)
+   return self:ImportNew(import, "\t")
+end
+
+function His:ImportNew (import, delimiter)
    wipe(private.errorList)
    local data = {strsplit("\n", import)}
    -- Validate the input
-   if not private:ValidateHeader(data[1]) then
-      return private.errors.malformed_header()
+   if not private:ValidateHeader(data[1], delimiter) then
+      private.errors.malformed_header()
+      print(data[1])
+      return
    end
    local line, lines = {}, {}
    for i = 2, #data do
-      line = self:ExtractLine(data[i], ",")
-      if #line ~= 21 then  print("Error:", i, #line, data[i]) end
+      line = self:ExtractLine(data[i], delimiter)
+      if #line ~= 24 then  print("Error:", i, #line, data[i]) end
       private:ValidateLine(i, line)
       tinsert(lines, line)
    end
@@ -43,8 +64,27 @@ function His:ImportCSV (import)
    -- Time to rebuild
    -- Clear errors and make it ready to be used again
    wipe(private.errorList)
+   local rebuilt = self:RebuildData(lines)
+   if self:DoErrorCheck() then return end
+   data = self:ConvertRebuiltDataToLootDBFormat(rebuilt)
+
+   printtable(data)
+end
+
+function His:ConvertRebuiltDataToLootDBFormat (rebuilt)
+   local data = {}
+   for _,v in ipairs(rebuilt) do
+      if not data[v.winner] then data[v.winner] = {} end
+      local lenght = #data[v.winner] + 1
+      data[v.winner][lenght] = v
+      data[v.winner][lenght].winner = nil
+   end
+   return data
+end
+
+function His:RebuildData (lines)
    local rebuilt = {}
-   for i = 1, 2 do
+   for i = 1, #lines do
       rebuilt[i] = {
          winner = "",
          lootWon = "",
@@ -53,41 +93,40 @@ function His:ImportCSV (import)
          instance = "",
          boss = "",
          votes = 0,
-         itemReplaced1 = "",
-         itemReplaced2 = "",
+         itemReplaced1 = nil,
+         itemReplaced2 = nil,
          response = "",
          responseID = "",
-         color = {},       -- no import, can be extracted from response
+         color = {},
          class = "",
          isAwardReason = false,
-         difficultyID = 0, -- needs extraction
-         mapID = 0,        -- no import
-         groupSize = 0,    -- no import
-         note = "",
+         difficultyID = 0,
+         mapID = 0,
+         groupSize = 0,
+         note = nil,
          id = "",
          owner = "Unknown",
-         typeCode = "", -- Probably not
+         -- typeCode = "", -- Probably not
          iClass = 0,
          iSubClass = 0
       }
       -- Each function has the responsibility to set each field themselves.
-      private:RebuildPlayerName(lines[i], rebuilt[i])
-      private:RebuildTime(lines[i], rebuilt[i])
-      private:RebuildLootWon(lines[i], rebuilt[i])
+      private:RebuildPlayerName(lines[i], rebuilt[i], i)
+      private:RebuildTime(lines[i], rebuilt[i], i)
+      private:RebuildLootWon(lines[i], rebuilt[i], i)
       -- we don't care too much about response
-      t.response = data[8]
-      t.votes = data[9] -- nor votes
-      t.class = data[10] -- Class is validated and can be used as is
-      private:RebuildInstance(lines[i], rebuilt[i])
-      t.boss = data[12] -- Don't care
-      private:RebuildReplacedGear(lines[i], rebuilt[i])
-      private:RebuildResponseID(lines[i], rebuilt[i])
-      t.isAwardReason = data[16]
-      t.note = data[20]
-      private:RebuildOwner(lines[i], rebuilt[i])
-
+      rebuilt[i].response = lines[i][8]
+      rebuilt[i].votes = lines[i][9] and tonumber(lines[i][9]) -- nor votes
+      rebuilt[i].class = lines[i][10] -- Class is validated and can be used as is
+      private:RebuildInstance(lines[i], rebuilt[i], i)
+      rebuilt[i].boss = lines[i][12] -- Don't care
+      private:RebuildReplacedGear(lines[i], rebuilt[i], i)
+      private:RebuildResponseID(lines[i], rebuilt[i], i)
+      rebuilt[i].isAwardReason = lines[i][19] and lines[i][19]:lower() == "true"
+      rebuilt[i].groupSize = lines[i][15] and tonumber(lines[i][15])
+      rebuilt[i].note = lines[i][23] ~= "" and lines[i][23] or nil
    end
-   if self:DoErrorCheck() then return end
+   return rebuilt
 end
 
 function His:DoErrorCheck ()
@@ -126,15 +165,46 @@ function His:ExtractLine (input, delimiter)
    return ret
 end
 
-function private:RebuildPlayerName (data, t)
-   t.winner = addon:UnitName(data[1] or "")
-   t.owner = addon:UnitName(data[20] or "") or t.owner
-   if not t.winner or t.winner == "" then
-      self:AddError(0, t.winner, "Could not restore playername for ".. data[1])
+function His:CommaEscapeTSV (data)
+   return data:gsub("\t(([^,\t\"]*,[^,\t\"]+)+)\t", "\t\"%1\"\t")
+end
+
+function private:RebuildResponseID(data,t, line)
+   local responseID = data[18]
+   if responseID:match("%d") then responseID = tonumber(responseID) end
+   -- Must be valid, i.e. check every single type of responses
+   for type in pairs(addon.responses) do
+      if addon.responses[type][responseID] then
+         -- also override response
+         t.response = addon.responses[type][responseID].text
+         t.color = addon.responses[type][responseID].color
+         t.responseID = responseID
+         return
+      end
+   end
+   -- Invalid
+   self:AddError(line, responseID, "Not a valid response of your current settings.")
+end
+
+function private:RebuildReplacedGear(data, t,line)
+   local gear1, gear2 = data[16], data[17]
+   if gear1 and gear1 ~= "" then
+      t.itemReplaced1 = select(2, self:GetItemInfo(gear1,nil,nil, line))
+   end
+   if gear2 and gear2 ~= "" then
+      t.itemReplaced2 = select(2, self:GetItemInfo(gear2, nil,nil, line))
    end
 end
 
-function private:RebuildTime (data, t)
+function private:RebuildPlayerName (data, t, line)
+   t.winner = addon:UnitName(data[1] or "")
+   t.owner = data[24] and data[24] ~= "Unknown" and addon:UnitName(data[24] or "") or t.owner
+   if not t.winner or t.winner == "" then
+      self:AddError(line, t.winner, "Could not restore playername for ".. data[1])
+   end
+end
+
+function private:RebuildTime (data, t, line)
    local dato, time, id = data[2], data[3], data[4]
    if not dato and not time and not id then
       -- Not provided, just use current time
@@ -173,31 +243,155 @@ function private:RebuildTime (data, t)
       private.idCount = private.idCount + 1
    else
       -- Should never happen
-      return self:AddError(0, string.format("%s|%s|%s", dato, time, id), "Could not recreate time.")
+      return self:AddError(line, string.format("%s|%s|%s", dato, time, id), "Could not recreate time.")
    end
 end
 
-function private:RebuildLootWon(data ,t)
-   local itemName, itemID, itemString = data[5],data[6],data[7]
+function private:GetItemInfo (name, id, itemString, line)
    local useForInfo = ""
    -- Pick the first we get, in most informative order:
    if itemString then
       useForInfo = itemString
-   elseif itemName then
-      useForInfo = itemName
-   elseif itemID then
-      useForInfo = itemID
-   else
-      return self:AddError(0, string.format("%s|%s|%s", itemName, itemID, itemString), "Could not extract the winning item from this info.")
+   elseif name then
+      if string.find(name, "%[") then -- We can't use brackets
+         useForInfo = string.sub(name, 2, -2)
+      else
+         useForInfo = name
+      end
+   elseif id then
+      useForInfo = id
+   end
+   if not useForInfo or useForInfo == "" then
+      return self:AddError(line, string.format("%s|%s|%s", tostring(name), tostring(id), tostring(itemString)), "Could not extract the winning item from this info.")
    end
 
    local iname, ilink, _, _, _, _, _, _,_, _, _, itemClassID, itemSubClassID = GetItemInfo(useForInfo)
    if not iname then
-      return self:AddError(0, useForInfo, "GetItemInfo() returned nil. Check inputs.")
+      return self:AddError(line, useForInfo, "GetItemInfo() returned nil. Check inputs.")
    end
+   return iname, ilink, itemClassID, itemSubClassID
+end
+
+function private:RebuildLootWon(data ,t, line)
+   local itemName, itemID, itemString = data[5],data[6],data[7]
+   local iname, ilink, itemClassID, itemSubClassID = self:GetItemInfo(itemName, itemID, itemString, line)
+   if not iname then return self:AddError(line, string.format("%s|%s|%s", tostring(itemName), tostring(itemID), tostring(itemString)), "Error rebuilding lootWon") end
    t.lootWon = ilink
    t.iClass = itemClassID
    t.iSubClass = itemSubClassID
+end
+
+local mapIDsToText = {
+   [469] = "Blackwing Lair",
+   [409] = "Molten Core",
+   [509] = "Ruins of Ahn'Qiraj",
+   [531] = "Temple of Ahn'Qiraj",
+   [564] = "Black Temple",
+   [565] = "Gruul's Lair",
+   [534] = "Hyjal Summit",
+   [532] = "Karazhan",
+   [544] = "Magtheridon's Lair",
+   [548] = "Serpentshrine Cavern",
+   [580] = "Sunwell Plateau",
+   [550] = "Tempest Keep",
+   [631] = "Icecrown Citadel",
+   [533] = "Naxxramas",
+   [249] = "Onyxia's Lair",
+   [616] = "The Eye of Eternity",
+   [615] = "The Obsidian Sanctum",
+   [724] = "The Ruby Sanctum",
+   [649] = "Trial of the Crusader",
+   [603] = "Ulduar",
+   [624] = "Vault of Archavon",
+   [757] = "Baradin Hold",
+   [669] = "Blackwing Descent",
+   [967] = "Dragon Soul",
+   [720] = "Firelands",
+   [671] = "The Bastion of Twilight",
+   [754] = "Throne of the Four Winds",
+   [1009] = "Heart of Fear",
+   [1008] = "Mogu'shan Vaults",
+   [1136] = "Siege of Orgrimmar",
+   [996] = "Terrace of Endless Spring",
+   [1098] = "Throne of Thunder",
+   [1228] = "Highmaul",
+   [1205] = "Blackrock Foundry",
+   [1448] = "Hellfire Citadel",
+   [1520] = "The Emerald Nightmare",
+   [1648] = "Trial of Valor",
+   [1530] = "The Nighthold",
+   [1676] = "Tomb of Sargeras",
+   [1712] = "Antorus, the Burning Throne",
+   [1861] = "Uldir",
+   [2070] = "Battle of Dazar'alor",
+   [2096] = "Crucible of Storms",
+   [2164] = "The Eternal Palace",
+}
+
+local diffIDToText = {
+   [1] = "Normal",
+   [2] = "Heroic",
+   [3] = "10 Player",
+   [4] = "25 Player",
+   [5] = "10 Player (Heroic)",
+   [6] = "25 Player (Heroic)",
+   [7] = "Looking For Raid",
+   [8] = "Mythic Keystone",
+   [9] = "40 Player",
+   [11] = "Heroic Scenario",
+   [12] = "Normal Scenario",
+   [14] = "Normal",
+   [15] = "Heroic",
+   [16] = "Mythic",
+   [17] = "Looking For Raid",
+   [18] = "Event",
+   [19] = "Event",
+   [20] = "Event Scenario",
+   [23] = "Mythic",
+   [24] = "Timewalking",
+   [25] = "World PvP Scenario",
+   [29] = "PvEvP Scenario",
+   [30] = "Event",
+   [32] = "World PvP Scenario",
+   [33] = "Timewalking",
+   [34] = "PvP",
+   [38] = "Normal",
+   [39] = "Heroic",
+   [40] = "Mythic",
+   [45] = "PvP",
+   [147] = "Normal",
+   [149] = "Heroic",
+}
+
+function private:RebuildInstance(data, t, line)
+   local instance, diffID, mapID = data[11], data[13], data[14]
+   if mapID and diffID then
+      mapID = tonumber(mapID)
+      diffID = tonumber(diffID)
+      if mapIDsToText[mapID] and diffIDToText[diffID] then
+         t.instance = mapIDsToText[mapID] .."-".. diffIDToText[diffID]
+         t.mapID = mapID
+         t.difficultyID = diffID
+      end
+   elseif mapID and not diffID and not instance then
+      mapID = tonumber(mapID)
+      t.instance = mapIDsToText[mapID]
+      t.mapID = mapID
+      t.difficultyID = nil
+   elseif instance then
+      if string.find(instance, "-") then
+         mapID = mapID or tInvert(mapIDsToText)[(string.split(instance, "-"))]
+         diffID = diffID or tInvert(diffIDToText[select(2,string.split(instance, "-"))])
+      else
+         mapID = mapID or tInvert(mapIDsToText)[instance]
+      end
+      t.instance = instance
+      t.mapID = mapID
+      t.difficultyID = diffID
+   else
+      self:AddError(line, string.format("%s|%s|%s", tostring(instance), tostring(diffID), tostring(mapID)), "Could not recreate instance info.")
+   end
+
 end
 
 private.errors = {
@@ -208,10 +402,12 @@ private.errors = {
 
 function private:AddError (lineNum, value, desc)
    tinsert(private.errorList, {lineNum, value, desc})
+   return nil
 end
 
-function private:ValidateHeader (header)
-   return header == "player, date, time, id, item, itemID, itemString, response, votes, class, instance, boss, gear1, gear2, responseID, isAwardReason, rollType, subType, equipLoc, note, owner"
+function private:ValidateHeader (header, delimiter)
+   local target = {"player", "date", "time", "id", "item", "itemID", "itemString", "response", "votes", "class", "instance", "boss", "difficultyID", "mapID", "groupSize", "gear1", "gear2", "responseID", "isAwardReason", "rollType", "subType", "equipLoc", "note", "owner"}
+   return header == table.concat(target,delimiter)
 end
 
 function private:ValidateLine (num, line)
@@ -236,7 +432,7 @@ private.validators = {
    end,
    function (num, input) -- id
       -- can be empty, a time string (seconds) or our id
-      return #input == 0 or input:match("%d+") or input:match("%d+%-%d+") or private:AddError(num, input,"Malformed time (nothing or 'hh:mm:ss')")
+      return #input == 0 or input:match("%d+") or input:match("%d+%-%d+") or private:AddError(num, input,"Malformed id (nothing, seconds or 'seconds-id'")
    end,
 
    function (num, input) -- item
@@ -271,6 +467,18 @@ private.validators = {
       -- optional
       return true
    end,
+   function (num, input) -- difficultyID
+      -- empty or number
+      return not input or input:match("[%d]+") or private:AddError(num, input, "Malformed difficultyID (must be a number or empty)")
+   end,
+   function (num, input) -- mapID
+      -- empty or number
+      return not input or input:match("[%d]+") or private:AddError(num, input, "Malformed mapID (must be a number or empty)")
+   end,
+   function (num, input) -- groupSize
+      -- empty or number
+      return not input or input:match("[%d]+") or private:AddError(num, input, "Malformed groupSize (must be a number or empty)")
+   end,
    function (num, input) -- gear1
       -- optional
       return true
@@ -285,7 +493,7 @@ private.validators = {
    end,
    function (num, input) -- isAwardReason
       -- optional, but TRUE/FALSE if not empty
-      return not input or (input and (input == "true" or input == "false")) or private:AddError(num,input, "Malformed isAwardReason - ('true' or 'false' or nothing)")
+      return not input or (input and (input:lower() == "true" or input:lower() == "false")) or private:AddError(num,input, "Malformed isAwardReason - ('true' or 'false' or nothing)")
    end,
    function (num, input) -- rollType
       -- optional
--- __tests/NewStuff/test_importer.lua
+++ __tests/NewStuff/test_importer.lua
@@ -1,5 +1,6 @@
 local lu = require("luaunit")
 dofile("../wow_api.lua")
+dofile("../wow_item_api.lua")
 dofile("../__load_libs.lua")
 dofile("../RCLootCouncilMock.lua")
 loadfile("../../Modules/lootHistory.lua")("", RCLootCouncil)
@@ -9,10 +10,15 @@ local testCSVData = loadfile("csv_test_data.lua")()
 local testPEData = loadfile("playerexport_test_data.lua")()
 local His = RCLootCouncil:GetModule("RCLootHistory")
 
-His:DetermineImportType(testCSVData)
+local type = His:DetermineImportType(testCSVData)
 His:DetermineImportType(testPEData)
 
-His:ImportCSV(testCSVData)
+print("Type:", type)
+if type == "tsv-csv" then
+   His:ImportTSV_CSV(testCSVData)
+else
+   His:ImportCSV(testCSVData)
+end
 local testLine = [=[Rageasaurus-Ravencrest,7/11/18,19:14:42,"[Khor, Hammer of the Corrupted]",160679,item:160679::::::::120:104::5:3:4799:1492:4786,Best in Slot,0,WARRIOR,Uldir-Heroic,MOTHER,"[Khor, Hammer of the Corrupted]",[Dismembered Submersible Claw],1,false,normal,Two-Handed Maces,Two-Hand,,Unkown]=]
 -- print(His:ExtractLine(testLine, ",")[12])
 -- print(printtable(His:ExtractLine(testLine, ",")))
