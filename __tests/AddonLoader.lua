ac044aa992220428da17b7d4db74b85940f42d56
evil_morfar <evil_morfar@hotmail.com>
2020-03-09 09:01:04 +0100

    Fully load entire addon for tests :)

--- __tests/AddonLoader.lua
+++ __tests/AddonLoader.lua
@@ -1,21 +1,37 @@
 --- Loads all the files!
-dofile "Locale/enUS.lua"
 local name,object = "RCLootCouncil", {}
 
 -- Load files
+dofile "Locale/enUS.lua"
 loadfile("Core/Defaults.lua")(name, object)
 loadfile("Core/Constants.lua")(name, object)
 loadfile("Core/CoreEvents.lua")(name, object)
-loadfile("core.lua")(name, object)
-loadfile("Utils/Utils.lua")(name, object)
-loadfile("Utils/ItemStorage.lua")(name,object)
 
+loadfile("core.lua")(name, object)
+loadfile("ml_core.lua")(name, object)
+
+loadfile("UI/UI.lua")(name, object)
+-- Consider loading widgets
+
+loadfile("Modules/lootFrame.lua")(name, object)
+loadfile("Modules/versionCheck.lua")(name, object)
+loadfile("Modules/votingFrame.lua")(name, object)
+loadfile("Modules/sessionFrame.lua")(name, object)
+loadfile("Modules/options.lua")(name, object)
+loadfile("Modules/History/lootHistory.lua")(name, object)
+loadfile("Modules/History/CSVImport.lua")(name, object)
+loadfile("Modules/TradeUI.lua")(name, object)
+
+loadfile("Utils/BackwardsCompat.lua")(name, object)
+loadfile("Utils/Utils.lua")(name, object)
+loadfile("Utils/trinketData.lua")(name, object)
+loadfile("Utils/tokenData.lua")(name, object)
+loadfile("Utils/ItemStorage.lua")(name,object)
+loadfile("Utils/autopass.lua")(name,object)
+loadfile("Utils/sync.lua")(name,object)
+loadfile("Utils/popups.lua")(name,object)
 
 -- Fire events
-print("ADDON_LOADED")
 WoWAPI_FireEvent("ADDON_LOADED", name)
-
 function IsLoggedIn () return true end
-
-print "PLAYER_LOGIN"
 WoWAPI_FireEvent("PLAYER_LOGIN")
