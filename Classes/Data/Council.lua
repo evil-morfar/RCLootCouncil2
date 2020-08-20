d12c57e8424842b03a5a71657d9b6050980316a8
evil_morfar <evil_morfar@hotmail.com>
2020-08-20 13:18:31 +0200

    Finished Data.Council

--- Classes/Data/Council.lua
+++ Classes/Data/Council.lua
@@ -4,26 +4,85 @@
 
 local _, addon = ...
 local Council = addon.Init("Data.Council")
+local Player = addon.Require "Data.Player"
+local TT = addon.Require "Utils.TempTable"
 
 local private = {
-   council = {}
+   council = {},
 }
 
-
 --- Returns the current council
-function Council:GetCouncil ()
+-- Council is Player = true
+function Council:Get ()
    return private.council
 end
 
-
-function Council:SetCouncil (args)
-   -- body...
+--- Sets the current council.
+-- Format is {
+--    [guid] = 'Data.Player'
+-- }
+function Council:Set (council)
+   private.council = council
 end
 
-function Council:GetCouncilForTransmit (args)
-   -- body...
+--- Returns the number of council members
+function Council:GetNum ()
+   return private:GetCouncilSize()
 end
 
-function Council:RestoreCouncilFromTransmit (args)
-   -- body...
+--- Adds a Player to the council.
+-- @args player A 'Data.Player' object
+-- @return The new council lenght
+function Council:Add (player)
+   if not player.name and player.guid then
+      error("Not a valid 'Player' object",2)
+   end
+   private.council[player.guid] = player
+   return private:GetCouncilSize()
+end
+
+--- Removes a Player from the council
+-- @args player A 'Data.Player' object
+-- @return The new council lenght
+function Council:Remove (player)
+   if not player.name and player.guid then
+      error("Not a valid 'Player' object",2)
+   end
+   private.council[player.guid] = nil
+   return private:GetCouncilSize()
+end
+
+--- Checks if a "Player" is in the council
+-- @args player A 'Data.Player' object
+-- @return boolean
+function Council:Contains (player)
+   return private.council[player.guid] ~= nil
+end
+
+--- Gets the council table optimized for comms.
+-- @return A "TempTable" with the council data.
+function Council:GetForTransmit ()
+   local tt = TT:Acquire()
+   for _, player in pairs(private.council) do
+      tt[player:GetForTransmit()] = true
+   end
+   return tt
+end
+
+--- Sets the currrent council to the received council.
+-- @returns Lenght of the new council.
+function Council:RestoreFromTransmit (council)
+   wipe(private.council)
+   for guid in pairs(council) do
+      local player = Player:Get(guid)
+      private.council[player.guid] = player
+   end
+   TT:Release(council)
+   return private:GetCouncilSize()
+end
+
+function private:GetCouncilSize ()
+   local size = 0
+   for _ in pairs(self.council) do size = size + 1 end
+   return size
 end
