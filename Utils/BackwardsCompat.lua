399c3b17df37f1997bb474d3721336e9ca8bb477
evil_morfar <evil_morfar@hotmail.com>
2020-10-20 16:53:05 +0200

    Fixed issues with Compat

--- Utils/BackwardsCompat.lua
+++ Utils/BackwardsCompat.lua
@@ -15,8 +15,14 @@ function Compat:Run()
         if
             v.version == "always" or
                 ((v.tVersion and
-                    addon:VersionCompare(addon.db.global.version, v.version, addon.db.globa.tVersion, v.tVersion)) or
-                    (addon:VersionCompare(addon.db.global.version, v.version) or not addon.db.global.version)) and
+                    addon.Utils:CheckOutdatedVersion(
+                        addon.db.global.version,
+                        v.version,
+                        addon.db.global.tVersion,
+                        v.tVersion
+                    ) == addon.VER_CHECK_CODES[3]) or
+                    (addon.Utils:CheckOutdatedVersion(addon.db.global.version, v.version) == addon.VER_CHECK_CODES[2] or
+                        not addon.db.global.version)) and
                     not v.executed
          then
             addon.Log("<Compat>", "Executing:", k, v.name or "no_name")
@@ -39,34 +45,38 @@ end
 --    func:    The function to execute if the version predicate is met. Called with the following parameters:
 --             (addon, addon.version, addon.db.global.version, addon.db.global.oldVersion)
 Compat.list = {
-  name = "v3.0 Update",
-  version = "3.0.0",
-  tVersion = "Beta.1",
-  func = function()
-      local db = addon:Getdb()
-      db.itemStorage = {} -- Just in case
-      db.council = {} -- Needs reset
-      -- All enabled by default, just removed for cleanup
-      db.altClickLooting = nil
-      db.autolootEverything = nil
-      db.autoLootOthersBoE = nil
-      db.saveBonusRolls = nil
-      db.allowNotes = nil
+    {
+        name = "v3.0 Update",
+        version = "3.0.0",
+        tVersion = "Beta.1",
+        func = function()
+            local db = addon:Getdb()
+            db.itemStorage = {} -- Just in case
+            db.council = {} -- Needs reset
+            -- All enabled by default, just removed for cleanup
+            db.altClickLooting = nil
+            db.autolootEverything = nil
+            db.autoLootOthersBoE = nil
+            db.saveBonusRolls = nil
+            db.allowNotes = nil
 
-      db.enabledButtons.CORRUPTED = nil
-      db.button.CORRUPTED = nil
-      db.responses.CORRUPTED = nil
+            db.enabledButtons.CORRUPTED = nil
+            db.buttons.CORRUPTED = nil
+            db.responses.CORRUPTED = nil
 
-      local global = addon.db.global
-      global.errors = {} -- just reset
-      global.localizedItemStatus = nil
-      -- Cleanup old reports
-      global["2.10.3"] = nil
-      global["2.11.0"] = nil
-      global["2.12.0"] = nil
-      global["2.12.1"] = nil
-      global["2.12.2"] = nil
-      global["2.13.1"] = nil
-      global["2.16.0"] = nil
-  end
+            local global = addon.db.global
+            global.errors = {} -- just reset
+            global.localizedItemStatus = nil
+            -- Cleanup old reports
+            global["2.10.3"] = nil
+            global["2.11.0"] = nil
+            global["2.12.0"] = nil
+            global["2.12.1"] = nil
+            global["2.12.2"] = nil
+            global["2.13.1"] = nil
+            global["2.16.0"] = nil
+
+            addon:ScheduleTimer("Print", 5, "Your Council has been reset as part of upgrading to v3.0")
+        end
+    }
 }
