689f180e7bd604e2105eebac429561ab2dc1cf70
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2022-10-18 12:47:07 +0200

    Fix ItemStorage:Multipred. More trade flow tests

--- .specs/Integration/Trade/FullTradeFlow.spec.lua
+++ .specs/Integration/Trade/FullTradeFlow.spec.lua
@@ -2,24 +2,26 @@ dofile(".specs/AddonLoader.lua").LoadToc("RCLootCouncil.toc")
 dofile ".specs/EmulatePlayerLogin.lua"
 
 local addon = RCLootCouncil
--- printtable(addon:Getdb())
+
+_G.IsInRaidVal = true
 
 describe("#Integration #Trade #FullTradeFlow", function()
-	local snapshot
-	local spyTradeComplete = spy.new()
-	local spyTradeWrongWinner = spy.new()
+	local spyTradeComplete
+	local spyTradeWrongWinner
 	local Comms = addon.Require "Services.Comms"
 
 	before_each(function()
-		snapshot = assert:snapshot()
-		Comms:Subscribe(addon.PREFIXES.MAIN, "trade_complete", spyTradeComplete)
+		spyTradeComplete = spy.new()
+		spyTradeWrongWinner = spy.new()
+		Comms:RegisterPrefix(addon.PREFIXES.MAIN)
 		Comms:Subscribe(addon.PREFIXES.MAIN, "trade_WrongWinner", spyTradeWrongWinner)
+		Comms:Subscribe(addon.PREFIXES.MAIN, "trade_complete", spyTradeComplete)
 
 	end)
 
 	after_each(function()
 		Comms:OnDisable()
-		snapshot:revert()
+		addon.ItemStorage:RemoveAllItems()
 	end)
 
 	it("should perform a full trade flow", function()
@@ -80,12 +82,41 @@ describe("#Integration #Trade #FullTradeFlow", function()
 		assert.spy(spyTradeComplete).was_called(1)
 	end)
 
-	it("trading duplicate items removes the proper one", function()
+	it("trading wrong winner should remove first instance of the item", function()
 		-- Get an item
 		local itemString = _G.Items_Array[1]
+		local itemString2 = _G.Items_Array[2]
 
-		-- Stub GetLootTable, as TradeUI relies on something being there.
-		stub(addon, "GetLootTable", {{link = itemString}})
+		-- Setup some items as if they've been awarded
+		stub(addon, "GetContainerItemTradeTimeRemaining", 3600)
+		addon.ItemStorage:New(itemString2, "to_trade", {recipient = "Player1-Realm1", session = 1}):Store()
+		addon.ItemStorage:New(itemString, "to_trade", {recipient = "Player1-Realm1", session = 1}):Store()
+		addon.ItemStorage:New(itemString, "to_trade", {recipient = "Player2-Realm1", session = 1}):Store()
+
+		assert.are.equal(3, #addon.ItemStorage:GetAllItems())
+
+		-- Setup vars normally handled when trading
+		addon.TradeUI.tradeItems = {itemString}
+		addon.TradeUI.tradeTarget = "Player3-Realm1"
+
+		WoWAPI_FireEvent("UI_INFO_MESSAGE", _G.LE_GAME_ERR_TRADE_COMPLETE)
+		-- Item 2 should now be removed
+		assert.are.equal(2, #addon.ItemStorage:GetAllItems())
+		local remaining = addon.ItemStorage:GetItem(itemString)
+		assert.are.same({recipient = "Player2-Realm1", session = 1}, remaining.args)
+
+		-- Test Comms
+		WoWAPI_FireUpdate(GetTime() + 10)
+		assert.spy(spyTradeWrongWinner).was_called(1)
+		assert.spy(spyTradeComplete).was_not_called()
+	end)
+
+	it("trading duplicate items removes the proper one", function()
+		printtable(addon.db.profile.itemStorage)
+		print (addon.db.profile.itemStorage[1])
+	   -- Get an item
+		local itemString = _G.Items_Array[1]
+
 		-- Setup some items as if they've been awarded
 		local item1 = addon.ItemStorage:New(itemString, "to_trade"):Store()
 		item1.time_remaining = 3600
@@ -104,6 +135,7 @@ describe("#Integration #Trade #FullTradeFlow", function()
 		-- Item 2 should now be removed
 		assert.are.equal(1, #addon.ItemStorage:GetAllItems())
 		local remaining = addon.ItemStorage:GetItem(itemString)
+		
 		assert.are.same({recipient = "Player1-Realm1", session = 1}, remaining.args)
 
 		-- Test Comms
