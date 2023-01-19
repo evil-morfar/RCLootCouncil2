8c769b3e2ea7654b10ca12fe500510892d80b581
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2023-01-19 12:06:24 +0100

    Fixed issue with changing history response and GetResponse without mldb

--- core.lua
+++ core.lua
@@ -2906,40 +2906,43 @@ end
 function RCLootCouncil:GetResponse(type, name)
 	-- REVIEW With proper inheritance, most of this should be redundant
 	-- Check if the type should be translated to something else
-	type = type and type or "default"
-	if self.mldb.responses and not self.mldb.responses[type] and self.BTN_SLOTS[type] and self.mldb.responses[self.BTN_SLOTS[type]] then
-		type = self.BTN_SLOTS[type]
-	end
-	-- FIXME If we use non default button without mldb (i.e. changing responses in loot history), this will fail.
-   if type == "default" or (self.mldb and not self.mldb.responses[type]) then -- We have a value if mldb is blank
-      if db.responses.default[name] or self.mldb.responses.default[name] then
-         return (self.mldb.responses and self.mldb.responses.default and self.mldb.responses.default[name]) or db.responses.default[name]
-      else
-         self:Debug("No db.responses.default entry for response:", name)
-         return self.defaults.profile.responses.default.DEFAULT -- Use default
-      end
-   else -- This must be supplied by the ml
-      if next(self.mldb) then
-         if self.mldb.responses[type] then
-            if self.mldb.responses[type][name] then
-               return self.mldb.responses[type][name]
-            else
-               self:Debug("No mldb.responses["..tostring(type).."] entry for response:".. tostring(name))
-            end
-         else
+	type = type or "default"
+	if self.mldb.responses and not self.mldb.responses[type] and self.BTN_SLOTS[type]
+		and self.mldb.responses[self.BTN_SLOTS[type]] then type = self.BTN_SLOTS[type] end
+
+	if type == "default" or (self.mldb and self.mldb.responses and not self.mldb.responses[type]) then -- We have a value if mldb is blank
+		if db.responses.default[name] or self.mldb.responses.default[name] then
+			return (self.mldb.responses and self.mldb.responses.default and self.mldb.responses.default[name])
+				or db.responses.default[name]
+		else
+			self:Debug("No db.responses.default entry for response:", name)
+			return self.defaults.profile.responses.default.DEFAULT -- Use default
+		end
+	else
+		if next(self.mldb) then
+			if self.mldb.responses[type] then
+				if self.mldb.responses[type][name] then
+					return self.mldb.responses[type][name]
+				else
+					self:Debug("No mldb.responses[" .. tostring(type) .. "] entry for response:" .. tostring(name))
+				end
+			else
 				-- This type is not enabled, so use default:
-            if db.responses.default[name] or self.mldb.responses.default[name] then
-               return self.mldb.responses.default and self.mldb.responses.default[name] or db.responses.default[name]
-            else
-               self:Debug("Unknown response entry", type, name)
-               return db.responses.default.DEFAULT -- Use default
-            end
-         end
-      else
-         self:Debug("No mldb for GetReponse: ".. tostring(type).. ", ".. tostring(name))
-      end
-   end
-   return {} -- Fallback
+				if db.responses.default[name] or self.mldb.responses.default[name] then
+					return self.mldb.responses.default and self.mldb.responses.default[name] or db.responses.default[name]
+				else
+					self:Debug("Unknown response entry", type, name)
+					return db.responses.default.DEFAULT -- Use default
+				end
+			end
+			-- See if we have local settings
+		elseif db.responses[type] and db.responses[type][name] then
+			return db.responses[type][name]
+		else
+			self:Debug("No db or mldb for GetReponse", type, name)
+		end
+	end
+	return {} -- Fallback
 end
 
 --- Returns the number of buttons of a specific type
