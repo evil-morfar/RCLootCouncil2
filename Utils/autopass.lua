9415167570750b6305049c92638135832ae9043c
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2024-02-24 17:56:01 +0100

    Initial work

--- Utils/autopass.lua
+++ Utils/autopass.lua
@@ -17,70 +17,79 @@ local autopassOverride = {
 local autopassTable = {
 	[Enum.ItemClass.Armor] = {
 
-		[Enum.ItemArmorSubclass.Cloth] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "ROGUE", "HUNTER", "SHAMAN",
+		[Enum.ItemArmorSubclass.Cloth] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "ROGUE", "HUNTER",
+			"SHAMAN",
 
-			"DEMONHUNTER", "EVOKER" },
-		[Enum.ItemArmorSubclass.Leather] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "HUNTER", "SHAMAN", "PRIEST", "MAGE", "WARLOCK",
+			"DEMONHUNTER", "EVOKER", },
+		[Enum.ItemArmorSubclass.Leather] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "HUNTER", "SHAMAN", "PRIEST", "MAGE",
+			"WARLOCK",
 
-			"EVOKER" },
+			"EVOKER", },
 
 		[Enum.ItemArmorSubclass.Mail] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "ROGUE", "PRIEST",
 			"MAGE", "WARLOCK",
 
-			"DEMONHUNTER" },
+			"DEMONHUNTER", },
 
-		[Enum.ItemArmorSubclass.Plate] = { "DRUID", "MONK", "ROGUE", "HUNTER", "SHAMAN", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER",
+		[Enum.ItemArmorSubclass.Plate] = { "DRUID", "MONK", "ROGUE", "HUNTER", "SHAMAN", "PRIEST", "MAGE", "WARLOCK",
+			"DEMONHUNTER",
 
-			"EVOKER" },
+			"EVOKER", },
 
-		[Enum.ItemArmorSubclass.Shield] = { "DEATHKNIGHT", "DRUID", "MONK", "ROGUE", "HUNTER", "PRIEST", "MAGE", "WARLOCK",
+		[Enum.ItemArmorSubclass.Shield] = { "DEATHKNIGHT", "DRUID", "MONK", "ROGUE", "HUNTER", "PRIEST", "MAGE",
+			"WARLOCK",
 
-			"DEMONHUNTER", "EVOKER" },
+			"DEMONHUNTER", "EVOKER", },
 
 	},
 	[Enum.ItemClass.Weapon] = {
-		[Enum.ItemWeaponSubclass.Axe1H] = { "DRUID", "PRIEST", "MAGE", "WARLOCK" },
+		[Enum.ItemWeaponSubclass.Axe1H] = { "DRUID", "PRIEST", "MAGE", "WARLOCK", },
 
-		[Enum.ItemWeaponSubclass.Axe2H] = { "DRUID", "ROGUE", "MONK", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER", "EVOKER" },
-
-
-		[Enum.ItemWeaponSubclass.Bows] = { "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "SHAMAN", "PRIEST", "MAGE", "WARLOCK",
-			"DEMONHUNTER", "WARRIOR", "EVOKER" },
-
-		[Enum.ItemWeaponSubclass.Crossbow] = { "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "SHAMAN", "PRIEST", "MAGE", "WARLOCK",
-
-			"DEMONHUNTER", "WARRIOR", "EVOKER" },
-
-		[Enum.ItemWeaponSubclass.Dagger] = { "DEATHKNIGHT", "PALADIN", "MONK", "DEMONHUNTER" },
-
-		[Enum.ItemWeaponSubclass.Guns] = { "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "SHAMAN", "PRIEST", "MAGE", "WARLOCK",
-
-			"DEMONHUNTER", "WARRIOR", "EVOKER" },
-
-		[Enum.ItemWeaponSubclass.Mace1H] = { "HUNTER", "MAGE", "WARLOCK", "DEMONHUNTER" },
-
-		[Enum.ItemWeaponSubclass.Mace2H] = { "MONK", "ROGUE", "HUNTER", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER", "EVOKER" },
+		[Enum.ItemWeaponSubclass.Axe2H] = { "DRUID", "ROGUE", "MONK", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER", "EVOKER", },
 
 
-		[Enum.ItemWeaponSubclass.Polearm] = { "ROGUE", "SHAMAN", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER", "EVOKER" },
+		[Enum.ItemWeaponSubclass.Bows] = { "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "SHAMAN", "PRIEST", "MAGE",
+			"WARLOCK",
+			"DEMONHUNTER", "WARRIOR", "EVOKER", },
+
+		[Enum.ItemWeaponSubclass.Crossbow] = { "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "SHAMAN", "PRIEST", "MAGE",
+			"WARLOCK",
+
+			"DEMONHUNTER", "WARRIOR", "EVOKER", },
+
+		[Enum.ItemWeaponSubclass.Dagger] = { "DEATHKNIGHT", "PALADIN", "MONK", "DEMONHUNTER", },
+
+		[Enum.ItemWeaponSubclass.Guns] = { "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "SHAMAN", "PRIEST", "MAGE",
+			"WARLOCK",
+
+			"DEMONHUNTER", "WARRIOR", "EVOKER", },
+
+		[Enum.ItemWeaponSubclass.Mace1H] = { "HUNTER", "MAGE", "WARLOCK", "DEMONHUNTER", },
+
+		[Enum.ItemWeaponSubclass.Mace2H] = { "MONK", "ROGUE", "HUNTER", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER", "EVOKER", },
+
+
+		[Enum.ItemWeaponSubclass.Polearm] = { "ROGUE", "SHAMAN", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER", "EVOKER", },
 
 
 		[Enum.ItemWeaponSubclass.Sword1H] = { "DRUID", "SHAMAN", "PRIEST", },
 
-		[Enum.ItemWeaponSubclass.Sword2H] = { "DRUID", "MONK", "ROGUE", "SHAMAN", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER", "EVOKER" },
+		[Enum.ItemWeaponSubclass.Sword2H] = { "DRUID", "MONK", "ROGUE", "SHAMAN", "PRIEST", "MAGE", "WARLOCK", "DEMONHUNTER", "EVOKER", },
 
 
-		[Enum.ItemWeaponSubclass.Staff] = { "DEATHKNIGHT", "PALADIN", "ROGUE", "DEMONHUNTER" },
+		[Enum.ItemWeaponSubclass.Staff] = { "DEATHKNIGHT", "PALADIN", "ROGUE", "DEMONHUNTER", },
 
-		[Enum.ItemWeaponSubclass.Wand] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "ROGUE", "HUNTER", "SHAMAN",
+		[Enum.ItemWeaponSubclass.Wand] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "ROGUE", "HUNTER",
+			"SHAMAN",
 
-			"DEMONHUNTER", "EVOKER" },
+			"DEMONHUNTER", "EVOKER", },
 
-		[Enum.ItemWeaponSubclass.Warglaive] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "ROGUE", "PRIEST", "MAGE",
+		[Enum.ItemWeaponSubclass.Warglaive] = { "WARRIOR", "DEATHKNIGHT", "PALADIN", "DRUID", "MONK", "ROGUE", "PRIEST",
+			"MAGE",
 
-			"WARLOCK", "HUNTER", "SHAMAN", "EVOKER" },
+			"WARLOCK", "HUNTER", "SHAMAN", "EVOKER", },
 
-		[Enum.ItemWeaponSubclass.Unarmed] = { "DEATHKNIGHT", "PALADIN", "PRIEST", "MAGE", "WARLOCK" }, -- Fist weapons
+		[Enum.ItemWeaponSubclass.Unarmed] = { "DEATHKNIGHT", "PALADIN", "PRIEST", "MAGE", "WARLOCK", }, -- Fist weapons
 
 	},
 }
@@ -89,34 +98,86 @@ local autopassTable = {
 -- NOTE I'm not sure if the return from C_ArtifactUI.GetRelicInfoByItemID() is localized
 -- @table relicTypes
 local relicTypes = {
-	BLOOD  = 'Blood',
-	SHADOW = 'Shadow',
-	IRON   = 'Iron',
-	FROST  = 'Frost',
-	FIRE   = 'Fire',
-	FEL    = 'Fel',
-	ARCANE = 'Arcane',
-	LIFE   = 'Life',
-	STORM  = 'Wind', -- Great job Blizzard -.-
-	HOLY   = 'Holy'
+	BLOOD  = "Blood",
+	SHADOW = "Shadow",
+	IRON   = "Iron",
+	FROST  = "Frost",
+	FIRE   = "Fire",
+	FEL    = "Fel",
+	ARCANE = "Arcane",
+	LIFE   = "Life",
+	STORM  = "Wind", -- Great job Blizzard -.-
+	HOLY   = "Holy",
 }
 
 --- The type of relic each class can use
 -- @table relics
 local relics = {
-	DEATHKNIGHT = { relicTypes.BLOOD, relicTypes.SHADOW, relicTypes.IRON, relicTypes.FROST, relicTypes.FIRE },
-	DEMONHUNTER = { relicTypes.FEL, relicTypes.SHADOW, relicTypes.IRON, relicTypes.ARCANE },
-	DRUID       = { relicTypes.ARCANE, relicTypes.LIFE, relicTypes.FIRE, relicTypes.BLOOD, relicTypes.FROST },
-	HUNTER      = { relicTypes.STORM, relicTypes.ARCANE, relicTypes.IRON, relicTypes.BLOOD, relicTypes.LIFE },
-	MAGE        = { relicTypes.ARCANE, relicTypes.FROST, relicTypes.FIRE },
-	MONK        = { relicTypes.LIFE, relicTypes.STORM, relicTypes.IRON, relicTypes.FROST },
-	PALADIN     = { relicTypes.HOLY, relicTypes.LIFE, relicTypes.FIRE, relicTypes.IRON, relicTypes.ARCANE },
-	PRIEST      = { relicTypes.HOLY, relicTypes.SHADOW, relicTypes.LIFE, relicTypes.BLOOD },
-	ROGUE       = { relicTypes.SHADOW, relicTypes.IRON, relicTypes.BLOOD, relicTypes.STORM, relicTypes.FEL },
-	SHAMAN      = { relicTypes.STORM, relicTypes.FROST, relicTypes.FIRE, relicTypes.IRON, relicTypes.LIFE },
-	WARLOCK     = { relicTypes.SHADOW, relicTypes.BLOOD, relicTypes.FIRE, relicTypes.FEL },
-	WARRIOR     = { relicTypes.IRON, relicTypes.BLOOD, relicTypes.SHADOW, relicTypes.FIRE, relicTypes.STORM },
+	DEATHKNIGHT = { relicTypes.BLOOD, relicTypes.SHADOW, relicTypes.IRON, relicTypes.FROST, relicTypes.FIRE, },
+	DEMONHUNTER = { relicTypes.FEL, relicTypes.SHADOW, relicTypes.IRON, relicTypes.ARCANE, },
+	DRUID       = { relicTypes.ARCANE, relicTypes.LIFE, relicTypes.FIRE, relicTypes.BLOOD, relicTypes.FROST, },
+	HUNTER      = { relicTypes.STORM, relicTypes.ARCANE, relicTypes.IRON, relicTypes.BLOOD, relicTypes.LIFE, },
+	MAGE        = { relicTypes.ARCANE, relicTypes.FROST, relicTypes.FIRE, },
+	MONK        = { relicTypes.LIFE, relicTypes.STORM, relicTypes.IRON, relicTypes.FROST, },
+	PALADIN     = { relicTypes.HOLY, relicTypes.LIFE, relicTypes.FIRE, relicTypes.IRON, relicTypes.ARCANE, },
+	PRIEST      = { relicTypes.HOLY, relicTypes.SHADOW, relicTypes.LIFE, relicTypes.BLOOD, },
+	ROGUE       = { relicTypes.SHADOW, relicTypes.IRON, relicTypes.BLOOD, relicTypes.STORM, relicTypes.FEL, },
+	SHAMAN      = { relicTypes.STORM, relicTypes.FROST, relicTypes.FIRE, relicTypes.IRON, relicTypes.LIFE, },
+	WARLOCK     = { relicTypes.SHADOW, relicTypes.BLOOD, relicTypes.FIRE, relicTypes.FEL, },
+	WARRIOR     = { relicTypes.IRON, relicTypes.BLOOD, relicTypes.SHADOW, relicTypes.FIRE, relicTypes.STORM, },
 }
+
+--- The stat(s) a weapon must have to be usable by a class.
+local requiredWeaponStatsForClass = {
+	WARRIOR = { "ITEM_MOD_STRENGTH_SHORT", },
+	PALADIN = { "ITEM_MOD_STRENGTH_SHORT", "ITEM_MOD_INTELLECT_SHORT", },
+	HUNTER = { "ITEM_MOD_AGILITY_SHORT", },
+	ROGUE = { "ITEM_MOD_AGILITY_SHORT", },
+	PRIEST = { "ITEM_MOD_INTELLECT_SHORT", },
+	DEATHKNIGHT = { "ITEM_MOD_STRENGTH_SHORT", },
+	SHAMAN = { "ITEM_MOD_INTELLECT_SHORT", "ITEM_MOD_AGILITY_SHORT", },
+	MAGE = { "ITEM_MOD_INTELLECT_SHORT", },
+	WARLOCK = { "ITEM_MOD_INTELLECT_SHORT", },
+	MONK = { "ITEM_MOD_INTELLECT_SHORT", "ITEM_MOD_AGILITY_SHORT", },
+	DRUID = { "ITEM_MOD_INTELLECT_SHORT", "ITEM_MOD_AGILITY_SHORT", },
+	DEMONHUNTER = { "ITEM_MOD_AGILITY_SHORT", },
+	EVOKER = { "ITEM_MOD_INTELLECT_SHORT", },
+}
+
+--- Returns false if the given item contains any of the stats required for the class
+--- as determined by the `requiredWeaponStatsForClass` list OR the item doesn't have any main stat.
+---@param itemLink ItemLink item to check
+---@param class ClassInfo.classFile Class to check against.
+local function ShouldAutoPassWeapon(itemLink, class)
+	-- Ensure class is valid
+	if not requiredWeaponStatsForClass[class] then
+		addon.Log:e("Invalid class in 'ShouldAutoPassWeapon'", class)
+		return false
+	end
+	
+	local stats = GetItemStats(itemLink)
+
+	-- Ensure item is loaded
+	if not stats then
+		addon.Log:d("Couldn't get stats for non-loaded item:", itemLink)
+		return false
+	end
+
+	-- Ensure it has any main stat
+	if not (stats.ITEM_MOD_STRENGTH_SHORT or
+			stats.ITEM_MOD_AGILITY_SHORT or
+			stats.ITEM_MOD_INTELLECT_SHORT) then
+		return false
+	end
+
+	-- Check each class stat against the item's stats
+	for _, stat in ipairs(requiredWeaponStatsForClass[class]) do
+		if stats[stat] then return false end
+	end
+
+	-- If all checks fails, then we must autopass the item
+	return true
+end
 
 --- Checks if the player should autopass on a given item.
 -- All params are supplied by the lootTable from the ML.
@@ -155,8 +216,18 @@ function RCLootCouncil:AutoPassCheck(link, equipLoc, typeID, subTypeID, classesF
 	end
 	if not tContains(autopassOverride, equipLoc) then
 		if autopassTable[typeID] and autopassTable[typeID][subTypeID] then
-			return tContains(autopassTable[typeID][subTypeID], class)
+			if tContains(autopassTable[typeID][subTypeID], class) then
+				return true
+			end
 		end
 	end
+
+	if typeID == Enum.ItemClass.Weapon then
+		-- TODO: Check weapon autopass settings
+		if ShouldAutoPassWeapon(link, class) then
+			return true
+		end
+	end
+
 	return false
 end
