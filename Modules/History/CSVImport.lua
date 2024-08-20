833f5c6ba0daa64f4f56ddb715ee8b3d3d857f51
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2024-08-20 17:05:40 +0200

    Added SoD mapIDs

--- Modules/History/CSVImport.lua
+++ Modules/History/CSVImport.lua
@@ -419,6 +419,12 @@ local mapIDsToText = {
    [2070] = "Battle of Dazar'alor",
    [2096] = "Crucible of Storms",
    [2164] = "The Eternal Palace",
+
+   -- SoD mapIDs
+   [90] = "Gnomeregan",
+   [109] = "Sunken Temple",
+   [2789] = "The Tainted Scar", -- Kazzak
+   [2791] = "Storm Cliffs",     -- Azuregos
 }
 
 local diffIDToText = {
@@ -454,6 +460,12 @@ local diffIDToText = {
    [45] = "PvP",
    [147] = "Normal",
    [149] = "Heroic",
+
+   -- SoD difficultyIDs
+   [186] = "40 Player", -- Onyxia/Kazzak/Azuregos 'Flex'
+   [198] = "Normal",    -- Gnomeregan 10player
+   [215] = "Normal",    -- Sunken Temple 20player
+   [226] = "20 Player", -- Molten Core
 }
 
 function private:RebuildInstance(data, t, line)
