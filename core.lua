277f049855639addc3637560e9303da7695dbed2
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2025-08-20 18:27:25 +0200

    Removed unused var

--- core.lua
+++ core.lua
@@ -1201,7 +1201,7 @@ function RCLootCouncil:SendLootAck(table, skip)
 		local session = v.session or k
 		if session > (skip or 0) then
 			hasData = true
-			local g1, g2 = self:GetGear(v.link, v.equipLoc, v.relic)
+			local g1, g2 = self:GetGear(v.link, v.equipLoc)
 			local diff = self:GetIlvlDifference(v.link, g1, g2)
 			toSend.gear1[session] = g1 and ItemUtils:GetItemStringClean(g1) or nil
 			toSend.gear2[session] = g2 and ItemUtils:GetItemStringClean(g2) or nil
