b283abba53236e61ebcb685857d1eec410fec54b
evil_morfar <10189576+evil-morfar@users.noreply.github.com>
2026-02-17 12:27:52 +0100

    CommsRestrictions update Store actual comms state in binary var Added tests

--- Classes/Services/CommsRestrictions.lua
+++ Classes/Services/CommsRestrictions.lua
@@ -11,17 +11,23 @@ CommsRestrictions.OnAddonRestrictionChanged = Subject.create()
 
 function CommsRestrictions:OnEnable()
 	addon:RegisterEvent("ADDON_RESTRICTION_STATE_CHANGED", self.ChangeEvent, self)
+	self.restrictionsEnabled = false
+	self.restrictions = 0x0
 end
 
 local AddOnRestrictionTypeReverse = tInvert(Enum.AddOnRestrictionType)
 local AddOnRestrictionStateReverse = tInvert(Enum.AddOnRestrictionState)
 function CommsRestrictions:ChangeEvent(_, type, state)
 	-- Combat seems to be the exception. Not sure about map - comms still work when it's enabled in instances.
-	self.restrictionsEnabled =
-		(state == Enum.AddOnRestrictionState.Active or state == Enum.AddOnRestrictionState.Activating) and
-		(type ~= Enum.AddOnRestrictionType.Combat and type ~= Enum.AddOnRestrictionType.Map)
-	addon.Log:d("Restriction:", AddOnRestrictionTypeReverse[type], AddOnRestrictionStateReverse[state], type, state,
-		self.restrictionsEnabled)
+	local newState = (state == Enum.AddOnRestrictionState.Active or state == Enum.AddOnRestrictionState.Activating) and 1 or 0
+	if newState == 1 then
+		self.restrictions = bit.bor(self.restrictions, bit.lshift(1, type))
+	else
+		self.restrictions = bit.band(self.restrictions, bit.bnot(bit.lshift(1, type)))
+	end
+	self.restrictionsEnabled = bit.band(self.restrictions, 0x6) ~= 0  -- 6 = 0b110, the bits for encounter and challenge mode
+	addon.Log:d("Restriction:", AddOnRestrictionTypeReverse[type], AddOnRestrictionStateReverse[state],
+		string.format("0x%x",self.restrictions), self.restrictionsEnabled)
 	self.OnAddonRestrictionChanged(self.restrictionsEnabled)
 end
 
